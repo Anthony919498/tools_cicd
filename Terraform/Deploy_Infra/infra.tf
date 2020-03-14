a5763f00061d5b4012c6a64965634d1dccf2f23d
cgache <50575572+cgache@users.noreply.github.com>
2020-03-14 21:26:04 +0100

    Update infra.tf

--- Terraform/Deploy_Infra/infra.tf
+++ Terraform/Deploy_Infra/infra.tf
@@ -13,6 +13,19 @@ variable "env" {
   default = "DEV"
 }
 
+data "aws_ami" "ubuntu" {
+most_recent = true
+ 
+filter {
+name = "name"
+values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+}
+ 
+filter {
+name = "virtualization-type"
+values = ["hvm"]
+}
+
 provider "aws" {
   region = "${var.aws-region}"
 }
